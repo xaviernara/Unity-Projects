f9c5216b49dc82b2168acf66e493ce778da700ec
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2018-03-14 07:03:25 -0700

    Finished my 1st project
    roll a ball

--- ROLL_BALL/Assets/Scripts/PlayerController.cs
+++ ROLL_BALL/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
-
+    private int count = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        setCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -22,4 +27,26 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count" + count.ToString();
+
+        if (count >= 11)
+        {
+            winText.text = "You Win!!!";
+        }
+
+    }
+
 }
